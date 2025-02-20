57f983cc0c7a7b5bc2826ba0835d9a5e1c30535c
Manuel Graziani <manuelgraziani42@gmail.com>
2025-02-20 23:16:09 +0100

    fix sonic sprite

--- src/main.js
+++ src/main.js
@@ -16,8 +16,8 @@ k.loadSprite("sonic", "graphics/sonic.png", {
   sliceX: 8,
   sliceY: 2,
   anims: {
-    run: {from: 0, to: 8, loop: true, speed: 50},
-    jump: {from: 9, to: 16, loop: true, speed: 100}
+    run: {from: 0, to: 7, loop: true, speed: 30},
+    jump: {from: 8, to: 15, loop: true, speed: 100}
   }
 })
 k.loadSprite("motobug", "graphics/motobug.png", {
