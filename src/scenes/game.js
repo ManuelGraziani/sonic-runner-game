8ae25d1780f7cb0d4f78688e54d7a9dde2b5f4d4
Manuel Graziani <manuelgraziani42@gmail.com>
2025-02-24 00:46:19 +0100

    backgrounds move vertically when sonic jumps

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -21,9 +21,9 @@ export default function game() {
     k.add([k.sprite("platforms"), k.pos(platformWidth * 4, 450), k.scale(4)]),
   ];
 
-  const sonic = makeSonic(k.vec2(200, 745))
-  sonic.setControls()
-  sonic.setEvents()
+  const sonic = makeSonic(k.vec2(200, 745));
+  sonic.setControls();
+  sonic.setEvents();
 
   let gameSpeed = 300;
   k.loop(1, () => {
@@ -35,8 +35,8 @@ export default function game() {
     k.opacity(0),
     k.area(),
     k.pos(0, 832),
-    k.body({isStatic: true}),
-  ])
+    k.body({ isStatic: true }),
+  ]);
 
   k.onUpdate(() => {
     if (bgPieces[1].pos.x < 0) {
@@ -47,6 +47,9 @@ export default function game() {
     bgPieces[0].move(-100, 0);
     bgPieces[1].moveTo(bgPieces[0].pos.x + bgPieceWidth * 2, 0);
 
+    bgPieces[0].moveTo(bgPieces[0].pos.x, -sonic.pos.y / 10 - 50);
+    bgPieces[1].moveTo(bgPieces[1].pos.x, -sonic.pos.y / 10 - 50);
+
     if (platforms[1].pos.x < 0) {
       platforms[0].moveTo(platforms[1].pos.x + platforms[1].width * 4, 450);
       platforms.push(platforms.shift());
@@ -54,5 +57,6 @@ export default function game() {
 
     platforms[0].move(-gameSpeed, 0);
     platforms[1].moveTo(platforms[0].pos.x + platforms[1].width * 4, 450);
+
   });
 }
